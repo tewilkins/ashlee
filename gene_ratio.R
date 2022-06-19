c4bb4165a01157c2fc2d1ac9ce45a1b61bb803f6
tewilkins <tewilkins@student.unimelb.edu.au>
2022-06-20 09:39:30 +1000

    push

--- gene_ratio.R
+++ gene_ratio.R
@@ -8,7 +8,12 @@ de_report$gene_ratio =  de_report$Significant / de_report$Annotated
 de_gg = de_report %>%
   ggplot(aes(x = gene_ratio, 
              y = Term, 
-             fill = Significant)) +
-  geom_bar(stat = "identity")
+             color = pvalues,
+             size = Significant)) +
+  geom_point(stat = "identity") +
+  scale_color_continuous(low = "red", high = "yellow") +
+  labs(color = "P-value", size = "Count", x = "Gene Ratio (Significant:Annotated)", y = "Gene Ontology Term") +
+  theme_bw()
 
 de_gg
+
